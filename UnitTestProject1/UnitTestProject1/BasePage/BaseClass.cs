e443b1c76d55e05988dff2395ed9c4969e376b5a
Simon Kunjumon <simonkunjumon@gmail.com>
2024-01-18 19:51:27 -0500

    branch commit

---
+++ UnitTestProject1/UnitTestProject1/BasePage/BaseClass.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Safari;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1.BasePage
+{
+
+   
+    public class BaseClass
+    {
+
+       
+
+       public IWebDriver driver;
+        [SetUp]
+        public void initialize()
+        {
+            driver = new ChromeDriver();
+            driver = new FirefoxDriver();
+            driver =new SafariDriver();
+            driver.Navigate().GoToUrl("https://www.saucedemo.com/v1/");
+            driver.Manage().Window.Maximize();
+           
+
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTestProject1/PageObjects/LoginPage.cs
@@ -0,0 +1,37 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Internal;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using UnitTestProject1.BasePage;
+namespace UnitTestProject1.PageObjects
+{
+  public class LoginPage
+    {
+       public IWebDriver driver;
+        
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+        By username = By.XPath("//input[@type='text' and @name='user-name']");
+        By password = By.XPath("//input[@type='password']");
+        By login = By.XPath("//input[@type='submit']");
+
+        public String Login(String x, String y) 
+        {   
+            Thread.Sleep(1000);
+            driver.FindElement(username).SendKeys(x);
+            driver.FindElement(password).SendKeys(y);
+            Thread.Sleep(1000);
+            driver.FindElement(login).Click();
+            String title = driver.Title;
+            return title;
+        }
+
+    }
+}
